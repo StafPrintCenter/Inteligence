08db6543a6d4bdf52d9132d250ce9b4e77c99236
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 11:03:38 +0100

    s

--- src/components/site/CopyButton.tsx
+++ src/components/site/CopyButton.tsx
@@ -1,6 +1,5 @@
 import { Check, Copy } from "lucide-react";
 import { useState } from "react";
-
 import { cn } from "@/lib/utils";
 
 export function CopyButton({ value, className }: { value: string; className?: string }) {
