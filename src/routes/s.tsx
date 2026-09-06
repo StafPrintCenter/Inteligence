af2b13a491816d971a5402d730dfc3cb5ccf5bbb
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 11:12:58 +0100

    s

--- src/routes/s.tsx
+++ src/routes/s.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { MessageSquare } from "lucide-react";
 import { useEffect, useState } from "react";
-import { CopyButton } from "@/components/site";
+import { CopyButton, SpcMobLogo } from "@/components/site";
 import { Markdown } from "@/components/spc/Markdown";
-import { SpcMobLogo } from "@/components/site/SpcLogo";
 import { Button } from "@/components/ui/button";
 import { decodeShare, type SharedPayload } from "@/lib/spc/share";
 import { SITE } from "@/data/site";
