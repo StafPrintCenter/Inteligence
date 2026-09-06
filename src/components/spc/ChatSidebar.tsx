f46dfef5bc25ccdfb75184becb520ff3b05f81ec
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 11:06:46 +0100

    s

--- src/components/spc/ChatSidebar.tsx
+++ src/components/spc/ChatSidebar.tsx
@@ -13,7 +13,7 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 
-import { SpcDeskLogo } from "@/components/spc/SpcLogo";
+import { SpcDeskLogo } from "@/components/site/SpcLogo";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
