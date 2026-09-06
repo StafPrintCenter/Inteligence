0052bda723a16e53b5b794c63421b29be5d5d6e5
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 12:58:21 +0100

    s

--- src/components/spc/ChatApp.tsx
+++ src/components/spc/ChatApp.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
-import { SignInGateDialog, WelcomeScreen } from "@/components/spc/chat";
-import { ChatHeader, MessageList, } from "@/components/chat/message";
+import { WelcomeScreen } from "@/components/spc/chat";
+import { ChatHeader, MessageList, SignInGateDialog, } from "@/components/chat/message";
 import { ChatSidebar } from "@/components/spc/ChatSidebar";
 import { ComposerBlock } from "@/components/spc/composer";
 import { DetailsPanel } from "@/components/spc/DetailsPanel";
