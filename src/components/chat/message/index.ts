11087e463365f948f5ed7c47d40ac3beffac185c
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 13:15:22 +0100

    s

--- src/components/chat/message/index.ts
+++
@@ -1,5 +0,0 @@
-export { ChatHeader } from "./Header";
-export { MessageItem } from "./Item";
-export { MessageList } from "./List";
-export { SignInGateDialog } from "./SignInGateDialog";
-export { WelcomeScreen } from "./WelcomeScreen";
