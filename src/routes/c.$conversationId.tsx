c1e84d376c5ff2c4863b076ffd9b815f9ec4c8df
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 13:24:07 +0100

    s

--- src/routes/c.$conversationId.tsx
+++ src/routes/c.$conversationId.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { ChatApp } from "@/components/conversation/ChatApp";
+import { ChatApp } from "@/components/conversation";
 import { SITE } from "@/data/site";
 
 const PAGE_TITLE = `Conversation · SPC Intelligence`;
