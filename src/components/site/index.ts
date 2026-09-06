217766470e319ab570b4c13bd6289585871c1a61
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 13:34:33 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -5,3 +5,4 @@ export { QrCodeAutoPanel } from "./QrCodeAutoPanel";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
 export { ChatSidebar } from "./ChatSidebar";
 export { DetailsPanel } from "./DetailsPanel";
+export { PreviewProvider } from "./PreviewPanel";
