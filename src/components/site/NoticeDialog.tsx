10994c9c59ede04eff3c2d4cfa6228797a372f95
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 11:22:37 +0100

    s

--- src/components/site/NoticeDialog.tsx
+++ src/components/site/NoticeDialog.tsx
@@ -1,5 +1,4 @@
 import { ShieldCheck } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
