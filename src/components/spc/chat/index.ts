432d27c3ba9d5b366b8fc838f8dc70ea99ac8239
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 12:22:54 +0100

    s

--- src/components/spc/chat/index.ts
+++ src/components/spc/chat/index.ts
@@ -1,4 +1,4 @@
-export { ChatHeader } from "./ChatHeader";
+export { ChatHeader } from "./Header";
 export { MessageItem } from "./MessageItem";
 export { MessageList } from "./MessageList";
 export { SignInGateDialog } from "./SignInGateDialog";
