5092eaa20372f6124019c5ba8eeb0f3c2df219af
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 13:24:12 +0100

    s

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { ChatApp } from "@/components/conversation/ChatApp";
+import { ChatApp } from "@/components/conversation";
 import { SITE } from "@/data/site";
 
 const PAGE_TITLE = `SPC Intelligence - Assistant IA de ${SITE.name}`;
