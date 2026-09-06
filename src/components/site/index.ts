ba909cc845cb25c1aa99bcf4ffac3cd74e89b3a5
steveasterafovo <steveasterafovo@gmail.com>
2026-09-06 11:30:38 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,4 +1,5 @@
 export { NoticeDialog } from "./NoticeDialog";
 export { CopyButton } from "./CopyButton";
 export { ShareDialog } from "./ShareDialog";
+export { QrCodeAutoPanel } from "./QrCodeAutoPanel";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
