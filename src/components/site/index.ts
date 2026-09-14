4f5e4b1b4396a88d59527f6fa7c936458fb7ec2e
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 14:47:33 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -6,3 +6,4 @@ export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
 export { ChatSidebar } from "./ChatSidebar";
 export { DetailsPanel } from "./DetailsPanel";
 export { PreviewProvider } from "./Preview/Provider";
+export { CookieConsent } from "./CookieConsent";
