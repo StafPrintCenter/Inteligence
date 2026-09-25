6f3cfe163b57d32989b70c3e087a6b7a719605c1
steveasterafovo <steveasterafovo@gmail.com>
2026-09-25 15:04:16 +0100

    s

--- src/components/site/ChatSidebar.tsx
+++ src/components/site/ChatSidebar.tsx
@@ -56,7 +56,6 @@ export function ChatSidebar({
     { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
     { label: "Facebook", href: SITE.socials.facebook, Icon: FacebookIcon },
     { label: "Instagram", href: SITE.socials.instagram, Icon: InstagramIcon },
-    { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
     { label: "X", href: SITE.socials.x, Icon: XIcon },
     { label: "WhatsApp", href: SITE.whatsappLink, Icon: WhatsAppIcon },
   ];
